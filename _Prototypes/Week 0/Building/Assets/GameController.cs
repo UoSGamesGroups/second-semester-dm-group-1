ad454527f82fba1711afad8e9389d89bb7b85463
William Terry <Tezza48is@aol.com>
2017-01-30 01:42:54 +0000

    First pass of stacking race prototype
    played on a tablet, players stack physics objects to build a tower to the middle before the opponent.
    Inspired by "Assembler 3".

---
+++ _Prototypes/Week 0/Building/Assets/GameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public Camera mBottomCamera, mTopCamera;
+    public StackingObject mCratePrefab;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))//create cube and drag at mouse's location
+        {
+            bool isBottom;
+            Vector2 mousePos = Input.mousePosition;
+            Vector2 spawnPos = new Vector2();
+            if (mousePos.y > Screen.height / 2)
+            {
+                spawnPos = mTopCamera.ScreenToWorldPoint(mousePos);
+                isBottom = false;
+            }
+            else
+            {
+                spawnPos = mBottomCamera.ScreenToWorldPoint(mousePos);
+                isBottom = true;
+            }
+            StackingObject newCrate = Instantiate(mCratePrefab, spawnPos, Quaternion.identity);
+            newCrate.Init(isBottom);
+
+            Debug.Log(mousePos);
+
+        }
+	}
+}
---
+++ _Prototypes/Week 0/Building/Assets/StackingObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class StackingObject : MonoBehaviour
+{
+
+    private const float GRAVITY_SCALE = 1;
+    private Rigidbody2D mRigid;
+
+    void Awake()
+    {
+        mRigid = GetComponent<Rigidbody2D>();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Init (bool isGravityDown)
+    {
+        mRigid.gravityScale = isGravityDown ? GRAVITY_SCALE : -GRAVITY_SCALE;
+    }
+}
