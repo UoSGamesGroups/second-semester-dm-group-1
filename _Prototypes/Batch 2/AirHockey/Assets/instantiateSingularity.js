5016bfc810befc7f28ceb97d7cad4fd9120402ba
Samuel Cook <s172776@ucs.ac.uk>
2017-03-04 16:24:02 +0000

    added singularity code

---
+++ _Prototypes/Batch 2/AirHockey/Assets/instantiateSingularity.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+var objectToInstantiate : GameObject;
+private var myCurrentObject : GameObject;
+public var _camera : GameObject;
+
+function Start()
+{
+
+}
+ 
+function Update(){
+    // if(Input.GetMouseButtonDown(0)){
+    //   myCurrentObject = Instantiate(objectToInstantiate, GetComponent.<Camera>().ScreenToWorldPoint(Input.mousePosition),Quaternion.identity);
+    // myCurrentObject.transform.position.z = 0;
+    //}
+    if(Input.GetMouseButton(0) && myCurrentObject)
+    {
+        myCurrentObject.transform.position =  _camera.GetComponent.<Camera>().ScreenToWorldPoint(Input.mousePosition);
+        myCurrentObject.transform.position.z = 0;
+    }
+    if(Input.GetMouseButtonUp(0) && myCurrentObject){
+        myCurrentObject = null;
+    }
+}
+function OnMouseDown ()
+{
+    if(Input.GetMouseButtonDown(0)){
+        myCurrentObject = Instantiate(objectToInstantiate, _camera.GetComponent.<Camera>().ScreenToWorldPoint(Input.mousePosition),Quaternion.identity);
+        myCurrentObject.transform.position.z = 0;
+    }
+}
---
+++ _Prototypes/Batch 2/AirHockey/Assets/singularity.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class singularity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(Vector3.back);
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Asteroid")
+        {
+            //collision.transform.parent = transform;
+            collision.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+            springJoint.connectedAnchor = this gameObject;
+            //collision.gameObject.GetComponent<DistanceJoint2D>().connectedBody = collision;
+            collision.transform.parent = transform;
+            //Release();
+        }
+    }
+   void Release ()
+    {
+        //yield return new WaitForSeconds(5);
+        gameObject.transform.parent = null;
+        gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+    }
+}
