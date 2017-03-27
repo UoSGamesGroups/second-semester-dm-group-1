9f339952dd5350a89c80debd8ca9d1d0c865b298
Samuel Cook <s172776@ucs.ac.uk>
2017-03-27 10:12:17 +0100

    added code for menu scene

--- _Project/Assets/Scripts/UI/UI_Button.cs
+++ _Project/Assets/Scripts/UI/UI_Button.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UI_Button : MonoBehaviour {
+   public Animator animMenu;
+   public Animator animCredits;
+   public Animator animInstructions;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -13,4 +16,33 @@ public class UI_Button : MonoBehaviour {
 	void Update () {
 		
 	}
+
+  //  public void MenuSlideIn()
+    //{
+      //  animMenu.SetBool("menu", true);
+    //}
+    //public void MenuSlideOut()
+    //{
+    //    animMenu.SetBool("menu", false);
+    //}
+    public void CreditsSlideIn()
+    {
+        animMenu.SetBool("menu", false);
+        animCredits.SetBool("credits", true);
+    }
+    public void CreditsSlideOut()
+    {
+        animCredits.SetBool("credits", false);
+        animMenu.SetBool("menu", true);
+    }
+    public void InstructionsSlideIn()
+    {
+        animMenu.SetBool("menu", false);
+        animInstructions.SetBool("Intructions", true);
+    }
+    public void InstructionsSlideOut()
+    {
+        animInstructions.SetBool("Intructions", false);
+        animMenu.SetBool("menu", true);
+    }
 }
