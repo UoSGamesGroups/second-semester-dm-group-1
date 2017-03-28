0bb32deeaa0404baf4ae68c1f6337f42ef3ab249
William Terry <Tezza48is@aol.com>
2017-03-28 18:12:56 +0100

    Instruction screen layout.

---
+++ _Project/Assets/PersistentObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Singleton that will not destroy on loading.
+public class PersistentObject : MonoBehaviour
+{
+    private static GameObject mSingleton;
+	// Use this for initialization
+	void Start ()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (mSingleton == null)
+        {
+            mSingleton = gameObject;
+        }
+        else
+        {
+            Destroy(gameObject);
+            Destroy(this);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
