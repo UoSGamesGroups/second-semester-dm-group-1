57bbed7b8bb56cc7de26cb3b0c177902e987f429
Samuel Cook <s172776@ucs.ac.uk>
2017-03-28 13:05:58 +0100

    added in music and screen constraint

---
+++ _Project/Assets/Scripts/Game/screenConstaraint.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenConstaraint : MonoBehaviour
+{
+
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+    }
+}
+
